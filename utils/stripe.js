53d6e6828d5df744c4e3fa9d8337335ad7b10229
Waleed Salah <walidsalah652@gmail.com>
2025-02-13 08:48:22 +0200

    fix connect with stripe issue from not loading env file

--- utils/stripe.js
+++ utils/stripe.js
@@ -1,6 +1,8 @@
+import { config } from "dotenv";
+config({ path: ".env.config" });
 import Stripe from "stripe";
 
-// console.log("api", process.env.STRIPE_SECRET);
+// console.log("api", process.env.BASE_URL);
 // if (!process.env.STRIPE_SECRET) {
 //   throw new Error("STRIPE_SECRET is missing in .env file");
 // }
